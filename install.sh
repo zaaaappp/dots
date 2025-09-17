d4e4f197f9599f1696fd7ec0b2d33ace70ad9e0e
zappp <zqppzbusiness@gmail.com>
2025-09-17 18:50:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+echo ""
 echo "--- Setting up yay"
+echo ""
 sleep 1
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 cd ..
-
+echo ""
 echo "--- Installing packages"
+echo ""
 sleep 1
 sudo pacman -S --noconfirm zsh sddm hyprland rofi-wayland swww kitty waybar cliphist playerctl pavucontrol wl-clipboard xdg-desktop-portal-hyprland git vim noto-fonts base-devel nerd-fonts lsd
 yay -S --noconfirm otf-minecraft
-
+echo ""
 echo "--- Setting up dots"
+echo ""
 sleep 1
 systemctl enable sddm.service
 chsh -s $(which zsh)
@@ -23,7 +27,9 @@ sudo cp -r vim/.vimrc ~/
 mkdir -p ~/Pictures/wallpapers/
 sudo cp -r assets/wallpapers ~/Pictures/
 sudo cp -r assets/windows-cursor /usr/share/icons/
-echo "--- Setup has finished
+echo ""
+echo "--- Installation has finished
+echo ""
 read -p "Do you want to restart the system now? (y/n): " RESTART
 
 if [[ "$RESTART" =~ ^[Yy]$ ]]; then
