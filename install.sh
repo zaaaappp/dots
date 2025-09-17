0f8ee0128fa5b1d024224885dbe3c4b9ae225642
zappp <zqppzbusiness@gmail.com>
2025-09-17 19:27:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo ""
 echo "--- Installing packages"
 echo ""
 sleep 1
-sudo pacman -S --noconfirm zsh sddm hyprland rofi-wayland swww kitty waybar cliphist playerctl pavucontrol wl-clipboard xdg-desktop-portal-hyprland git vim noto-fonts base-devel nerd-fonts lsd fastfetch
+sudo pacman -S --noconfirm zsh sddm hyprland rofi-wayland swww kitty waybar cliphist playerctl pavucontrol wl-clipboard xdg-desktop-portal-hyprland git vim noto-fonts base-devel nerd-fonts lsd fastfetch matugen swaync thunar cava btop
 yay -S --noconfirm otf-minecraft
 echo ""
 echo "--- Setting up dots"
@@ -32,12 +32,6 @@ sudo cp -r assets/windows-cursor /usr/share/icons/
 swww img ~/Pictures/wallpapers/941959.png
 matugen image ~/Pictures/wallpapers/941959.png
 echo ""
-echo "--- Installation has finished
+echo "--- Installation has finished"
 echo "Installation has successfully finished."
-echo "Restarting in 3 seconds"
-sleep 1
-echo "Restarting in 2 seconds"
-sleep 1
-echo "Restarting in 1 second"
-sleep 1
-sudo reboot now
+echo ""
