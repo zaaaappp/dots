2e2b00610b1ff2268bb64e3c12fdc6d37e77908b
zappp <zqppzbusiness@gmail.com>
2025-09-17 19:10:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,12 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 cd ..
+rm -rf yay
 echo ""
 echo "--- Installing packages"
 echo ""
 sleep 1
-sudo pacman -S --noconfirm zsh sddm hyprland rofi-wayland swww kitty waybar cliphist playerctl pavucontrol wl-clipboard xdg-desktop-portal-hyprland git vim noto-fonts base-devel nerd-fonts lsd
+sudo pacman -S --noconfirm zsh sddm hyprland rofi-wayland swww kitty waybar cliphist playerctl pavucontrol wl-clipboard xdg-desktop-portal-hyprland git vim noto-fonts base-devel nerd-fonts lsd fastfetch
 yay -S --noconfirm otf-minecraft
 echo ""
 echo "--- Setting up dots"
@@ -23,19 +24,12 @@ chsh -s $(which zsh)
 sudo cp -r .config/* ~/.config/
 sudo cp -r zsh/.zshrc ~/
 sudo cp -r zsh/.p10k.zsh ~/
+sudo cp -r zsh/powerlevel10k ~/
 sudo cp -r vim/.vimrc ~/
 mkdir -p ~/Pictures/wallpapers/
 sudo cp -r assets/wallpapers ~/Pictures/
 sudo cp -r assets/windows-cursor /usr/share/icons/
 echo ""
 echo "--- Installation has finished
-echo ""
-read -p "Do you want to restart the system now? (y/n): " RESTART
-
-if [[ "$RESTART" =~ ^[Yy]$ ]]; then
-    echo "Rebooting..."
-    sudo reboot now
-else
-    echo "Starting SDDM..."
-    sudo systemctl start sddm
-fi
+echo "Installation has successfully finished."
+echo "Restarting in 3 seconds"
